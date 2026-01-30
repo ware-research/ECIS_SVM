a1feacd82c9127e772f7c4700056d84d5ef3696c
Jason P Ware <71521264+ware-research@users.noreply.github.com>
2026-01-30 15:11:55 -0800

    Refactor SVM classification and visualization code

--- SVMclassify_public.py
+++ SVMclassify_public.py
@@ -1,3 +1,37 @@
+'''
+MIT License
+
+Copyright (c) 2024, Oregon Health & Science Univeristy
+
+Contributor(s): Jason Ware (warej@ohsu.edu)
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+Please consider citing!
+
+
+
+NOTE: This is a simplified example code for reference in manuscript submission. 
+A complete workflow including validation splits and further analysis will be 
+released with formal publication
+'''
+
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -98,3 +132,4 @@ y_pred = svm_classifier.predict(X)
 misclassification_percentage = (1 - accuracy_score(y, y_pred)) * 100
 
 print(f'Misclassification Percentage: {misclassification_percentage:.2f}%')
+
